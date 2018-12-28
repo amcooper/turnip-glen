b2c2d50031f127aca9d281a3c6b9a3c481f148cf
Adam Cooper <theadamcooper@protonmail.com>
2018-12-28 12:56:18 -0500

    Fragment containers - expansion - wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryRenderer } from "react-relay";
+import environment from "../environment";
 import ArticleList from "./ArticleList.jsx";
 
 class App extends React.Component {
   render() {
     return (
-      <ArticleList />
+      <QueryRenderer 
+        environment={environment}
+        query={graphql`
+          query AppQuery {
+            articles {
+              edges {
+                node {
+                  id
+                  headline
+                  subhed
+                  excerpt
+                  image_url
+                  publication_time
+                  authors {
+                    edges {
+                      node {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+        variables={{}}
+        render={({error, props}) => {
+          if (error) {
+            console.error(error);
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <ArticleList articles={props.articles} />
+          )
+        }}
+      />  
     )
   }
 }
