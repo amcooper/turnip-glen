e3c3c116470fc8f7481731a18f5676613036add2
Adam Cooper <theadamcooper@protonmail.com>
2019-02-28 10:16:18 -0500

    Starting comment mutation

---
+++ src/mutations/AddCommentMutation.js
@@ -0,0 +1,56 @@
+import { commitMutation, graphql } from 'react-relay';
+import { ConnectionHandler } from 'relay-runtime';
+
+const mutation = graphql`
+  mutation addComment($input: NewCommentInput!) {
+    addComment(input: $input) {
+      comment {
+        id
+        body
+        parent_comment_id
+        publication_time
+        author {
+          name
+        }
+      }
+    }
+  }
+`;
+
+// Completely uncertain about this 
+function sharedUpdater(store, newComment) {
+  const conn = ConnectionHandler.getConnection('CommentList_comments');
+  ConnectionHandler.insertEdgeAfter(conn, newComment);
+}
+
+let tempID = 0;
+
+function commit(environment, body, parent_comment_id, article_id, author_id) {
+  return commitMutation(environment, {
+    mutation,
+    variables: {
+      input: {
+        body,
+        parent_comment_id,
+        article_id,
+        author_id,
+        clientMutationId: String(tempID++)
+      }
+    },
+    updater: store => {
+      const payload = store.getRootField('addComment');
+      const newComment = payload.getLinkedRecord('comment');
+      sharedUpdater(store, newComment);
+    },
+    optimisticUpdater: store => {
+      const id = 'client:newComment:' + tempID++;
+      const node = store.create(id, 'Comment');
+      node.setValue(body, 'comment body');
+      node.setValue(id, 'id');
+      const newComment = store.create('client:newComment' + tempID++);
+      // unfinished
+    }
+  })
+}
+
+export default { commit };
