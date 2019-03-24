ae4aa1b510b3ce4f65fcc6fc745ae2033978c8cb
amcooper <theadamcooper@protonmail.com>
2019-03-24 14:30:06 -0400

    wip

--- src/mutations/AddCommentMutation.js
+++ src/mutations/AddCommentMutation.js
@@ -21,52 +21,26 @@ const mutation = graphql`
   }
 `;
 
-/*
- * Some doggerel to test out Syntastic
- *
-function plebSharedUpdater(store,dev) {
-	debugger;
-	let v = 0;
-	if ( v > 0 ) {
-		console.log("Pefrect.");
-	}
-}
-*
-*/
-
 // Completely uncertain about this 
-function sharedUpdater(store, newComment) {
+/* function sharedUpdater(store, newComment) {
   const conn = ConnectionHandler.getConnection('CommentList_comments');
   ConnectionHandler.insertEdgeAfter(conn, newComment);
 }
+*/
 
 let tempID = 0;
 
-function commit(environment, body, parent_comment_id, articleId, authorId) {
+function commit(environment, body, parent_comment_id, article_id, author_id) {
   return commitMutation(environment, {
     mutation,
     variables: {
       input: {
         body,
         parent_comment_id,
-        articleId,
-        authorId,
+        article_id,
+        author_id,
         clientMutationId: String(tempID++)
       }
-    },
-    updater: store => {
-      const payload = store.getRootField('addComment');
-      const newComment = payload.getLinkedRecord('comment');
-      sharedUpdater(store, newComment);
-    },
-    optimisticUpdater: store => {
-      const id = 'client:newComment:' + tempID++;
-      const node = store.create(id, 'Comment');
-      node.setValue(body, 'comment body');
-      node.setValue(id, 'id');
-      const newComment = store.create('client:newComment' + tempID++);
-      newComment.setLinkedRecord(node, 'node');
-      sharedUpdater(store, newComment);
     },
     onCompleted: (response, errors) => {
       console.log('Response received from server.')
