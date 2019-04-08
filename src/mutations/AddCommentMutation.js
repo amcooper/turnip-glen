775b8d1f38ec8b123926f2ac43a2c5cace53d3a6
Adam Cooper <amcooper@gmail.com>
2019-04-08 12:29:28 -0400

    Variable names

--- src/mutations/AddCommentMutation.js
+++ src/mutations/AddCommentMutation.js
@@ -2,51 +2,51 @@ import { commitMutation, graphql } from 'react-relay';
 import { ConnectionHandler } from 'relay-runtime';
 
 const mutation = graphql`
-  mutation AddCommentMutation($input: NewCommentInput!) {
-    addComment(input: $input) {
-      commentEdge {
-        __typename
-        cursor
-        node {
-          id
-          body
-          parent_comment_id
-          publication_time
-          author {
-            name
-          }
-        }
-      }
-    }
-  }
+	mutation AddCommentMutation($input: NewCommentInput!) {
+		addComment(input: $input) {
+			commentEdge {
+				__typename
+				cursor
+				node {
+					id
+					body
+					parent_comment_id
+					publication_time
+					author {
+						name
+					}
+				}
+			}
+		}
+	}
 `;
 
 // Completely uncertain about this 
 /* function sharedUpdater(store, newComment) {
-  const conn = ConnectionHandler.getConnection('CommentList_comments');
-  ConnectionHandler.insertEdgeAfter(conn, newComment);
+	const conn = ConnectionHandler.getConnection('CommentList_comments');
+	ConnectionHandler.insertEdgeAfter(conn, newComment);
 }
-*/
 
 let tempID = 0;
+*/
 
-function commit(environment, body, parent_comment_id, article_id, author_id) {
-  return commitMutation(environment, {
-    mutation,
-    variables: {
-      input: {
-        body,
-        parent_comment_id,
-        article_id,
-        author_id,
-        clientMutationId: String(tempID++)
-      }
-    },
-    onCompleted: (response, errors) => {
-      console.log('Response received from server.')
-    },
-    onError: error => console.error(error),
-  });
+function commit(environment, body, parentCommentId, articleId, authorId) {
+	return commitMutation(environment, {
+		mutation,
+		variables: {
+			input: {
+				body,
+				parentCommentId: null, // parentCommentId,
+				articleId,
+				authorId,
+				/* clientMutationId: String(tempID++) */
+			}
+		},
+		onCompleted: (response, errors) => {
+			console.log('Response received from server.')
+		},
+		onError: error => console.error(error),
+	});
 }
 
 export default { commit };
