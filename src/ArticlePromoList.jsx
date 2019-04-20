637fe010594f3d9520b41736868dd03ba2f35850
Adam Cooper <amcooper@gmail.com>
2019-04-20 17:32:18 -0400

    tabs to spaces

--- src/ArticlePromoList.jsx
+++ src/ArticlePromoList.jsx
@@ -4,24 +4,24 @@ import ArticlePromoListItem from "./ArticlePromoListItem.jsx";
 import "./ArticlePromoList.css";
 
 class ArticlePromoList extends React.Component {
-    render() {
-        return (
-            <ul className="ArticlePromoList">
-                {this.props.articles.edges.map((edge) => {
-                    return (
-                        <li className="ArticlePromoListItem" key={edge.node.id}>
-                            <ArticlePromoListItem id={edge.node.id} article={edge.node} />
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <ul className="ArticlePromoList">
+        {this.props.articles.edges.map((edge) => {
+          return (
+            <li className="ArticlePromoListItem" key={edge.node.id}>
+              <ArticlePromoListItem id={edge.node.id} article={edge.node} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default createFragmentContainer(
-    ArticlePromoList,
-    graphql`
+  ArticlePromoList,
+  graphql`
     fragment ArticlePromoList_articles on ArticleConnection {
       edges {
         node {
