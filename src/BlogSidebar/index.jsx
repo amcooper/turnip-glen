0dcbe774daf6092548a6d1c108f7254317e82b45
Adam Cooper <amcooper@gmail.com>
2019-05-06 08:17:01 -0400

    wip

--- src/BlogSidebar/index.jsx
+++ src/BlogSidebar/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import TagList from "../TagList/index.jsx";
 
-const BlogSidebar = () => {
-  return (
-    <div className="BlogSidebar-container">
-      <h4>sidebar</h4>
-      <TagList />
-    </div>
-  );
+class BlogSidebar extends React.Component {
+  render() {
+    return (
+      <div className="BlogSidebar-container">
+        <h4>sidebar</h4>
+        <TagList />
+      </div>
+    );
+  }
 };
 
 export default BlogSidebar;
