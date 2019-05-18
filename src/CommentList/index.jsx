396930813eef3ec23b078db85d796d56c91f0ef5
Adam Cooper <amcooper@gmail.com>
2019-05-18 08:48:58 -0400

    wip

--- src/CommentList/index.jsx
+++ src/CommentList/index.jsx
@@ -4,31 +4,31 @@ import "./styles.css";
 import Comment from "../Comment/index.jsx";
 
 class CommentList extends React.Component {
-    render () {
-        return (
-            <ul className="CommentList">
-                {this.props.comments.edges.map(edge => {
-                    return (
-                        <li className="Comment" key={edge.node.id}>
-                            <Comment comment={edge.node} />
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    }
+  render () {
+    return (
+      <ul className="CommentList">
+        {this.props.comments.edges.map(edge => {
+          return (
+            <li className="Comment" key={edge.node.id}>
+              <Comment comment={edge.node} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default createFragmentContainer(
-    CommentList,
-    graphql`
-    fragment CommentList_comments on CommentConnection {
-      edges {
-        node {
-          id
-          ...Comment_comment 
-        }
-      }
+  CommentList,
+  graphql`
+  fragment CommentList_comments on CommentConnection {
+    edges {
+    node {
+      id
+      ...Comment_comment 
     }
+    }
+  }
   `
 );
